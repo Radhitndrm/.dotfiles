6e68d45c03efa3a3948dda8196e44362f0becd88
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-14 23:59:51 +0800

    none

--- nvim/lua/ndrm/plugins/colorscheme.lua
+++ nvim/lua/ndrm/plugins/colorscheme.lua
@@ -10,21 +10,19 @@
 -- }
 
 return {
-  'shaunsingh/moonlight.nvim',
-  priority = 1000,
-  lazy = false,
-  config = function()
-    -- Aktifkan tema moonlight
-    vim.cmd.colorscheme 'moonlight'
+	"shaunsingh/moonlight.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("moonlight")
 
-    -- Tambahan untuk transparansi
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
-  end,
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+	end,
 }
