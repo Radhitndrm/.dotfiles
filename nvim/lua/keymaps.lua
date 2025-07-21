5d4cf81cfdb83ac48fbf8a27a660f3082231bf29
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-21 18:53:28 +0800

    removing ll

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -35,7 +35,6 @@ vim.keymap.set("n", "<M-r>", ":LspRestart<CR>")
 
 vim.keymap.set("n", ";;", "mzA;<Esc>`z", { desc = "Tambah semicolon di akhir baris" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("n", "ll", "A", { noremap = true, silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
