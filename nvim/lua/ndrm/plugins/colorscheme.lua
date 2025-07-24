18b95bb6910c6fbee89c8026800a9f54a699b5c3
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-24 23:13:51 +0800

    editing

--- nvim/lua/ndrm/plugins/colorscheme.lua
+++ nvim/lua/ndrm/plugins/colorscheme.lua
@@ -1,21 +1,18 @@
--- return {
---   'navarasu/onedark.nvim',
---   priority = 1000,
---   config = function()
---     require('onedark').setup {
---       style = 'darker',
---     }
---     require('onedark').load()
---   end,
--- }
-
 return {
-	"shaunsingh/moonlight.nvim",
+	"neanias/everforest-nvim",
+	version = false,
 	priority = 1000,
 	lazy = false,
 	config = function()
-		vim.cmd.colorscheme("moonlight")
+		require("everforest").setup({
+			background = "soft", -- bisa diganti dengan "medium" atau "hard"
+			transparent_background = true,
+			italics = true,
+		})
 
+		vim.cmd.colorscheme("everforest")
+
+		-- Atur transparansi manual untuk elemen tertentu (seperti moonlight)
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -26,3 +23,21 @@ return {
 		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 	end,
 }
+
+-- return {
+-- 	"shaunsingh/moonlight.nvim",
+-- 	priority = 1000,
+-- 	lazy = false,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("moonlight")
+--
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+-- 	end,
+-- }
--- nvim/lua/ndrm/plugins/neo-tree.lua
+++ nvim/lua/ndrm/plugins/neo-tree.lua
@@ -1,27 +1,55 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['<leader>e'] = 'close_window',
-        },
-      },
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- ikon
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>e", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["<leader>e"] = "close_window",
+				},
+			},
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+			},
+		},
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
+
+		-- Highlight groups NeoTree dengan background transparan
+		local neotree_transparent_groups = {
+			"NeoTreeNormal",
+			"NeoTreeNormalNC",
+			"NeoTreeEndOfBuffer",
+			"NeoTreeWinSeparator",
+			"NeoTreeCursorLine",
+			"NeoTreeDirectoryName",
+			"NeoTreeDirectoryIcon",
+			"NeoTreeFileName",
+			"NeoTreeFloatBorder",
+			"NeoTreeTitleBar",
+		}
+
+		for _, group in ipairs(neotree_transparent_groups) do
+			vim.api.nvim_set_hl(0, group, { bg = "none" })
+		end
+
+		-- Warna khusus untuk nama file (sesuai tema alam/hutan)
+		vim.api.nvim_set_hl(0, "NeoTreeFileName", { fg = "#A3BE8C" }) -- hijau daun lembut
+		vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = "#88C0D0" }) -- biru muda untuk file terbuka
+		vim.api.nvim_set_hl(0, "NeoTreeFileNameModified", { fg = "#EBCB8B" }) -- kuning hangat untuk file diubah
+
+		-- Opsional: atur warna separator agar lembut dan transparan
+		vim.cmd([[highlight NeoTreeWinSeparator guibg=none guifg=#5c6a72]]) -- warna netral dari everforest
+	end,
 }
