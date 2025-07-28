de2a71d21029dd40b582438bcae54ec05ab79169
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-28 23:00:06 +0800

    editing

--- nvim/lua/ndrm/plugins/colorscheme.lua
+++ nvim/lua/ndrm/plugins/colorscheme.lua
@@ -1,34 +1,46 @@
 return {
-	"folke/tokyonight.nvim",
+	"EdenEast/nightfox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-			terminal_colors = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = false },
-				functions = {},
-				variables = {},
-				sidebars = "transparent",
-				floats = "transparent",
+		require("nightfox").setup({
+			options = {
+				transparent = true,
+				terminal_colors = true,
+				dim_inactive = false,
 			},
-			sidebars = { "qf", "help", "packer" },
-			day_brightness = 0.2,
-			dim_inactive = false,
+			palettes = {
+				nightfox = {
+					bg0 = "#0a0a0a", -- background gelap (cursorline)
+					orange = "#ff6b00", -- oranye cerah (tmux orange)
+					blue = "#5ad0ff", -- biru cerah (tmux blue)
+					green = "#a67c52", -- cokelat kayu
+					yellow = "#d6a864", -- krem keemasan
+					red = "#e94b28", -- api/asap
+					cyan = "#7ca9c0", -- langit biru
+					gray = "#3f4a62", -- garis antar pane
+				},
+			},
+			styles = {
+				comments = "italic",
+				keywords = "NONE",
+				functions = "NONE",
+				variables = "NONE",
+			},
+			inverse = {
+				match_paren = true,
+			},
 		})
 
-		vim.cmd("colorscheme tokyonight")
+		vim.cmd("colorscheme nightfox")
 
-		-- ✨ Custom warna dari palet yang kamu inginkan
+		-- ✨ Custom highlight tambahan
 		vim.api.nvim_set_hl(0, "Comment", { fg = "#7ca9c0", italic = true }) -- langit biru
 		vim.api.nvim_set_hl(0, "Statement", { fg = "#e94b28", bold = true }) -- api/asap
 		vim.api.nvim_set_hl(0, "Function", { fg = "#d6a864" }) -- krem keemasan
 		vim.api.nvim_set_hl(0, "Identifier", { fg = "#a67c52" }) -- cokelat kayu
 		vim.api.nvim_set_hl(0, "Type", { fg = "#ff6740" }) -- oranye cerah
-		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1e293b" }) -- biru tua
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#0a0a0a" }) -- background cursor line gelap
 		vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3f4a62", bg = "none" }) -- garis antar pane
 	end,
 }
