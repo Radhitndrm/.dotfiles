1facf022c4464ff5615bf0c1a7a9c84a70fb412f
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-29 14:42:08 +0800

    lualine

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -1,75 +1,77 @@
-require('lazy').setup({
-  'NMAC427/guess-indent.nvim',
+require("lazy").setup({
+	"NMAC427/guess-indent.nvim",
 
-  require 'ndrm.plugins.gitsigns',
+	require("ndrm.plugins.gitsigns"),
 
-  require 'ndrm.plugins.telescope',
+	require("ndrm.plugins.telescope"),
 
-  require 'ndrm.plugins.lspconfig',
+	require("ndrm.plugins.lspconfig"),
 
-  require 'ndrm.plugins.vim-surround',
+	require("ndrm.plugins.vim-surround"),
 
-  require 'ndrm.plugins.vim-matchit',
+	require("ndrm.plugins.vim-matchit"),
 
-  require 'ndrm.plugins.conform',
+	require("ndrm.plugins.conform"),
 
-  require 'ndrm.plugins.cmp',
+	require("ndrm.plugins.cmp"),
 
-  require 'ndrm.plugins.highlight-color',
+	require("ndrm.plugins.highlight-color"),
 
-  -- require 'ndrm.plugins.vim-abolish',
+	-- require 'ndrm.plugins.vim-abolish',
 
-  require 'ndrm.plugins.autotag',
+	require("ndrm.plugins.autotag"),
 
-  require 'ndrm.plugins.namespace',
+	require("ndrm.plugins.namespace"),
 
-  require 'ndrm.plugins.harpoon',
+	require("ndrm.plugins.harpoon"),
 
-  require 'ndrm.plugins.copilot',
+	require("ndrm.plugins.copilot"),
 
-  require 'ndrm.plugins.copilot-chat',
+	require("ndrm.plugins.copilot-chat"),
 
-  require 'ndrm.plugins.vim-visual-multi',
+	require("ndrm.plugins.vim-visual-multi"),
 
-  require 'ndrm.plugins.toggleterm',
+	require("ndrm.plugins.toggleterm"),
 
-  require 'ndrm.plugins.commentstring',
+	require("ndrm.plugins.commentstring"),
 
-  require 'ndrm.plugins.toggleterm',
+	require("ndrm.plugins.toggleterm"),
 
-  require 'ndrm.plugins.presence',
+	require("ndrm.plugins.presence"),
 
-  require 'ndrm.plugins.colorscheme',
+	require("ndrm.plugins.colorscheme"),
 
-  require 'ndrm.plugins.todo-comments',
+	require("ndrm.plugins.todo-comments"),
 
-  require 'ndrm.plugins.mini',
+	require("ndrm.plugins.mini"),
 
-  require 'ndrm.plugins.treesitter',
+	require("ndrm.plugins.treesitter"),
 
-  require 'ndrm.plugins.lint',
+	require("ndrm.plugins.lint"),
 
-  require 'ndrm.plugins.autopairs',
+	require("ndrm.plugins.autopairs"),
 
-  require 'ndrm.plugins.neo-tree',
+	require("ndrm.plugins.neo-tree"),
+
+	require("ndrm.plugins.lualine"),
 }, {
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+	ui = {
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
