bd4c3283d1257f99ca208720a361c50538123ffa
Radhitndrm <radhit.andromeda@gmail.com>
2025-08-01 22:35:21 +0800

    adding web dev icons

--- nvim/lua/ndrm/plugins/neo-tree.lua
+++ nvim/lua/ndrm/plugins/neo-tree.lua
@@ -3,7 +3,10 @@ return {
 	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		{
+			"nvim-tree/nvim-web-devicons",
+			enabled = true,
+		},
 		"MunifTanjim/nui.nvim",
 	},
 	cmd = "Neotree",
